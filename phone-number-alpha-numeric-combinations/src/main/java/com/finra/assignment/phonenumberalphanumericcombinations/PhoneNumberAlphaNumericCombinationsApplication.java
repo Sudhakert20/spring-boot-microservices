42b28765eb77c39fba761a4bb39d80faacf69bdb
Sudhaker Reddy Teegala <STeegala@Sudhakers-MacBook-Pro.local>
2020-04-17 21:48:59 -0400

    Initial bootstrap commit of phone number alpha numeric combinations.

---
+++ phone-number-alpha-numeric-combinations/src/main/java/com/finra/assignment/phonenumberalphanumericcombinations/PhoneNumberAlphaNumericCombinationsApplication.java
@@ -0,0 +1,13 @@
+package com.finra.assignment.phonenumberalphanumericcombinations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneNumberAlphaNumericCombinationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneNumberAlphaNumericCombinationsApplication.class, args);
+	}
+
+}
---
+++ phone-number-alpha-numeric-combinations/src/test/java/com/finra/assignment/phonenumberalphanumericcombinations/PhoneNumberAlphaNumericCombinationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finra.assignment.phonenumberalphanumericcombinations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneNumberAlphaNumericCombinationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
